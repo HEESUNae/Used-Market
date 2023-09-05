cb6a16962ad58622521a20f755e3cd8abc73d1f6
HEESUNae <heesun9456@gmail.com>
2023-09-05 20:16:22 +0900

    feat: 상품업로드 UI 생성

---
+++ src/app/products/upload/page.tsx
@@ -0,0 +1,64 @@
+'use client';
+import Button from '@/components/Button';
+import Container from '@/components/Container';
+import Heading from '@/components/Heading';
+import Input from '@/components/Input';
+import React, { useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+
+const ProductUploadPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      title: '',
+      description: '',
+      category: '',
+      latitude: 33.5563,
+      longitude: 126.79581,
+      imageSrc: '',
+      price: 1,
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {};
+  return (
+    <Container>
+      <div className="max-w-screen-lg mx-auto">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-8">
+          <Heading title="Product Upload" subtitle="upload yout product" />
+          <Input id="title" label="Title" disabled={isLoading} register={register} errors={errors} required />
+          <Input
+            id="description"
+            label="Description"
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+          />
+          <Input
+            id="price"
+            label="Price"
+            formatPrice
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+          />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">{/* category */}</div>
+          {/* kakaomap */}
+          <Button label="상품 생성하기" />
+        </form>
+      </div>
+    </Container>
+  );
+};
+
+export default ProductUploadPage;
