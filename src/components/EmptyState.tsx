e0ebe6f5cbd0517b9e5a8f6f16ff34c950799626
HEESUNae <heesun9456@gmail.com>
2023-09-09 16:16:52 +0900

    feat: Category 컴포넌트 생성

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import Heading from './Heading';
 import Button from './Button';
@@ -16,7 +17,7 @@ const EmptyState = ({
 }: EmptyStateProps) => {
   const router = useRouter();
   return (
-    <div className="h-[60vh] flex flex-col gap-2 justify-content items-center">
+    <div className="h-[60vh] flex flex-col gap-2 justify-center items-center">
       <Heading center title={title} subtitle={subtitle} />
       <div className="w-48 mt-4">
         {showReset && <Button outline label="모든 필터 제거" onClick={() => router.push('/')} />}
